6a71fa53d0422d07264c1afbf2b556272555dc74
Muhammad Haris Shaikh <MUHAMMADHARIS.SHAIKH@ncratleos.com>
2024-01-08 22:22:43 +0500

    initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'angular-localization';
+export class AppComponent implements OnInit {
+  emailAddress!: string;
+  experience!: string;
+  user!: { loggedInUser: string };
+
+  constructor(
+    public translate: TranslateService
+  ) {
+    // Register translation languages
+    translate.addLangs(['en', 'es', 'fr', 'ar']);
+    // Set default language
+    translate.setDefaultLang('en');
+
+    this.getTitleString();
+  }
+
+  ngOnInit(): void {
+    this.user = { loggedInUser: 'John Steve' }; // Defining static value for demo
+  }
+
+  //Switch language
+  translateLanguageTo(lang: string) {
+    this.translate.use(lang);
+  }
+
+  getTitleString() {
+    // asynchronous - gets translations then completes.
+    this.translate.get(['USER_INFO.EMAIL_ADDRESS', 'USER_INFO.RELEVANT_EXPERIENCE'])
+      .subscribe(translations => {
+        this.emailAddress = translations['USER_INFO.EMAIL_ADDRESS'];
+        this.experience = translations['USER_INFO.RELEVANT_EXPERIENCE'];
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+// Factory function required during AOT compilation
+export function httpTranslateLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
